cff63c5288502ff6f1f774b7f74bc2ba6a599a1c
trinhngoc2709 <nindaphi@gmail.com>
2022-05-07 22:01:16 +0700

    Modify signIn signUp

--- Backend/models/song.js
+++ Backend/models/song.js
@@ -5,15 +5,15 @@ const HttpError = require("../models/http-error");
 
 module.exports = class Song{
     static getImageBySongID = tryCatchBlock(async (id) => {
-        const [resultSet] = await database.execute(`SELECT id_image_storage from Song where song_id = '${id}'`);
+        const [resultSet] = await database.execute(`SELECT idImageStorage from Song where songID = '${id}'`);
         return resultSet;
     });
     static getMusicBySongID = tryCatchBlock(async (id) => {
-        const [resultSet] = await database.execute(`SELECT id_music_storage from Song where song_id = '${id}'`);
+        const [resultSet] = await database.execute(`SELECT idMusicStorage from Song where songID = '${id}'`);
         return resultSet;
     });
     static getAssetsBySongID = tryCatchBlock(async (id) => {
-        const [resultSet] = await database.execute(`SELECT id_image_storage,id_music_storage from Song where song_id = '${id}'`);
+        const [resultSet] = await database.execute(`SELECT idImageStorage,idMusicStorage from Song where songID = '${id}'`);
         return resultSet;
     });
     static getTop100 = tryCatchBlock(async (type) => {
