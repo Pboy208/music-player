14710941de02e8a04c7168a36db9fabbfe21cbcd
trinhngoc2709 <nindaphi@gmail.com>
2022-07-08 23:35:51 +0700

    API lay profile cua nguoi dung

--- Backend/routes/user-router.js
+++ Backend/routes/user-router.js
@@ -4,10 +4,12 @@ const userController = require("../controllers/user-controller.js");
 const checkAuth = require("../middleware/check-auth");
 
 router.use(checkAuth);
+router.get("/explore/profile/:userId",userController.exploreProfileUser);
 router.get("/users", userController.getUsers);
 router.get("/:userId", userController.getUserById);
 router.get("/profile/:userId",userController.getProfile);
 router.post("/profile",userController.updateProfile);
+
 //router.use(checkAuth);
 // router.get("/:name", userController.searchByName);
 
