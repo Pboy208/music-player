a39e37ce5deac5ef03d2be5ff9f52399e08d83d5
Pboy208 <phuonghoanghaha@gmail.com>
2022-07-12 23:06:46 +0700

    update

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -3,12 +3,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import songReducer from './songSlice';
 import authReducer from './authSlice';
-import albumReducer from './albumSlice';
 import httpErrorHandlerMiddleware from './httpErrorHandlerMiddleware';
 import httpStatusHandlerMiddleware from './httpStatusHandlerMiddleware';
 
 export const store = configureStore({
-  reducer: { song: songReducer, auth: authReducer, album: albumReducer },
+  reducer: { song: songReducer, auth: authReducer },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       httpStatusHandlerMiddleware,
