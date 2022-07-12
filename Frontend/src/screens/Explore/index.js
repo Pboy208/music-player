116eddc32a1623e9d1a8a156bc31712f7d503aef
Le Ngoc Kien <lengockien2000@gmail.com>
2022-07-12 21:33:27 +0700

    update

--- Frontend/src/screens/Explore/index.js
+++ Frontend/src/screens/Explore/index.js
@@ -1,66 +1,10 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import SongCardList from 'components/Song/SongCardList';
-import AlbumCardList from 'components/Album/AlbumCardList';
 import ArtistCardList from 'components/Artist/ArtistCardList';
 import { Link } from 'react-router-dom';
 
 function Explore() {
-  const [ exploreSong, setExploreSong ] = useState([]);
-  const [ exploreAlbum, setExploreAlbum ] = useState([]);
-  const [ exploreArtist, setExploreArtist ] = useState([]);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow",
-    };
-
-    fetch("http://localhost:8888/song/explore/song", requestOptions)
-      .then((response) => response.json())
-      .then((result) => result.data.map((val, index, array) => array[array.length - 1 - index]))
-      .then((result) => setExploreSong(result))
-      .catch((error) => console.log("error", error));
-
-    fetch("http://localhost:8888/album/explore/album", requestOptions)
-      .then((response) => response.json())
-      .then((result) => result.data.map((val, index, array) => array[array.length - 1 - index]))
-      .then((result) => setExploreAlbum(result))
-      .catch((error) => console.log("error", error));
-
-    fetch("http://localhost:8888/song/explore/artist", requestOptions)
-      .then((response) => response.json())
-      .then((result) => result.data.map((val, index, array) => array[array.length - 1 - index]))
-      .then((result) => setExploreArtist(result))
-      .catch((error) => console.log("error", error));
-  }, []);
-  
-  if (!exploreSong || !exploreAlbum || !exploreArtist) return null;
-
-  const transformedSong = exploreSong.map(
-    ({
-      name,
-      artistID,
-      nameArtist,
-      songID,
-      urlImage,
-      urlMusic
-    }) => 
-    ({ 
-      authorId: artistID,
-      name,
-      author: nameArtist,
-      songId: songID,
-      urlImage,
-      urlMusic
-    }));
-
-  return (
+    return (
     <Wrapper className="card aligned-center">
       <h1>Explorer</h1>
       <NewSong>
@@ -68,18 +12,16 @@ function Explore() {
           <h3>New release</h3>
           <Link style={{ marginLeft: "20px",padding: "5px",fontSize: "16px", fontWeight: "600"}} to="/explore/song">More</Link>
         </div>
-        <SongCardList exploreSong={transformedSong}/>
+        <SongCardList/>
       </NewSong>
       <Artist>
         <h3>New artist feature</h3>
-        <ArtistCardList exploreArtist={exploreArtist}/>
+        <ArtistCardList/>
       </Artist>
-      <NewAlbum>
+      {/* <NewAlbum>
         <h3>New album arrived</h3>
-        <AlbumCardList exploreAlbum={exploreAlbum} />
-      </NewAlbum>
-
-      
+        <AlbumCardList />
+      </NewAlbum> */}
     </Wrapper>
   );
 }
